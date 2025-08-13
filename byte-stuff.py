e279ff313d6d0ce72b1760f2a93e96736643f8c0
anonymise <vnrita27@gmail.com>
2025-08-13 14:37:32 +0530

    byte v1

--- byte-stuff.py
+++ byte-stuff.py
@@ -1,21 +1,24 @@
+FLAG = '~'  # Frame delimiter
+ESC = '}'   # Escape character
+
 def byte_stuffing(data):
-    s = ""
+    s = FLAG  # Start of frame
     for ch in data:
-        if ch == '~':   # Frame delimiter
-            s += '}' + '~'
-        elif ch == '}':  # Escape character
-            s += '}' + '}'
+        if ch == FLAG:
+            s += ESC + FLAG
+        elif ch == ESC:
+            s += ESC + ESC
         else:
             s += ch
+    s += FLAG  # End of frame
     return s
 
 
 def byte_unstuffing(stuffed_data):
     us = ""
-    i = 0
-    while i < len(stuffed_data):
-        if stuffed_data[i] == '}':
-            # Next character is either ~ or }
+    i = 1  # Skip first FLAG
+    while i < len(stuffed_data) - 1:  # Stop before last FLAG
+        if stuffed_data[i] == ESC:
             i += 1
             us += stuffed_data[i]
         else:
@@ -26,8 +29,8 @@ def byte_unstuffing(stuffed_data):
 
 # Example usage:
 data = "Hello~World}Test~"
-print("Original Data: ", data)
+print("Original Data:    ", data)
 stuffed_data = byte_stuffing(data)
-print("Byte Stuffed: ", stuffed_data)
+print("Byte Stuffed:     ", stuffed_data)
 unstuffed_data = byte_unstuffing(stuffed_data)
-print("Byte Unstuffed: ", unstuffed_data)
+print("Byte Unstuffed:   ", unstuffed_data)
